c826b29524028e48362e2e7c6e8ffedd15efb328
EugenFM <eugenfm22@gmail.com>
2024-10-30 14:54:14 -0500

    Added try-catch

--- js/main.js
+++ js/main.js
@@ -1,29 +1,49 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
 document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch() {
+async function getFetch() {
   const choice = document.querySelector("input").value;
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
-
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      console.log(data.name);
-      console.log(data.subclasses);
-      document.querySelector("h2").innerText = data.name;
-      document.querySelector("h3").innerText = data.classes[0].name;
-
-      // get the subclasses names and add them to the DOM (in the ul)
-      data.subclasses.forEach((obj) => {
-        console.log(obj.name);
-        const li = document.createElement("li");
-        li.textContent = obj.name;
-        let subclassesList = document.querySelector("ul");
-        subclassesList.appendChild(li);
-      });
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
+  const url = `https://www.dnd5eapi.co/api/subclasses/?name=${choice}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    console.log(data.results);
+    data.results.forEach((obj) => {
+      const li = document.createElement("li");
+      li.textContent = obj.name;
+      document.querySelector("ul").appendChild(li);
     });
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
 }
+
+// function getFetch() {
+//   const choice = document.querySelector("input").value;
+//   const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
+
+// //   fetch(url)
+// //     .then((res) => res.json())
+// //     .then((data) => {
+// //       console.log(data);
+// //       console.log(data.name);
+// //       console.log(data.subclasses);
+// //       document.querySelector("h2").innerText = data.name;
+// //       document.querySelector("h3").innerText = data.classes[0].name;
+
+// //       // get the subclasses names and add them to the DOM (in the ul)
+// //       data.subclasses.forEach((obj) => {
+// //         console.log(obj.name);
+// //         const li = document.createElement("li");
+// //         li.textContent = obj.name;
+// //         let subclassesList = document.querySelector("ul");
+// //         subclassesList.appendChild(li);
+// //       });
+// //     })
+// //     .catch((err) => {
+// //       console.log(`error ${err}`);
+// //     });
+// // }
